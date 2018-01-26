d1d74cf28b2d338b29559bc213c3018af41e0f44
Parker Lusk <parkerclusk@gmail.com>
2018-01-25 22:29:59 -0700

    camera model update; allow passing of latlon to copter_sitl.sh

--- scripts/copter_sitl.sh
+++ scripts/copter_sitl.sh
@@ -2,9 +2,13 @@
 
 # sleep 10
 
+# Pass in lat lon of starting point (origin in Gazebo)
+# with syntax: 40.267987,-111.635558 (rock canyon park)
+LATLON=$1
+
 source ~/.bash_profile
 
-CMD="sim_vehicle.py -v ArduCopter -f gazebo-iris -l 40.267941,-111.635381,0,0 --map --console"
+CMD="sim_vehicle.py -v ArduCopter -f gazebo-iris -l $LATLON,0,0 --map --console"
 xterm -fa monospace -fs 12 -n CopterSITL -T CopterSITL -hold -geometry 100x20 -e $CMD
 
 # This is a hacky way of doing cleanup, but it works for now.
