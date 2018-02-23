913112b448031a2f47f999164adffaa6b9f5b24f
Parker Lusk <parkerclusk@gmail.com>
2018-02-23 13:50:12 -0700

    better process cleanup on SIGINT

--- scripts/copter_sitl.sh
+++ scripts/copter_sitl.sh
@@ -8,11 +8,8 @@ LATLON=$1
 
 source ~/.bash_profile
 
+# Catch CTRL+C and do SITL cleanup.
+trap '{ echo "Cleaning up copter_sitl.sh"; killall xterm && killall mavproxy.py; }' INT
+
 CMD="sim_vehicle.py -v ArduCopter -f gazebo-iris -l $LATLON,0,0 --map --console"
 xterm -fa monospace -fs 12 -n CopterSITL -T CopterSITL -hold -geometry 100x20 -e $CMD
-
-# This is a hacky way of doing cleanup, but it works for now.
-# sim_vehicle.py kicks off a new xterm window for arducopter and
-# mavproxy is running as well for the SITL stuff.
-pkill xterm
-pkill mavproxy.py
