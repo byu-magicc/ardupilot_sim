06681bb6351504a81e324f1fed86378f46656bea
Parker Lusk <parkerclusk@gmail.com>
2018-03-05 12:47:06 -0700

    cleaned up sitl launch script

--- scripts/copter_sitl.sh
+++ scripts/copter_sitl.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# sleep 10
-
 # Pass in lat lon of starting point (origin in Gazebo)
 # with syntax: 40.267987,-111.635558 (rock canyon park)
 LATLON=$1
-
-source ~/.bash_profile
 
 # Catch CTRL+C and do SITL cleanup.
 trap '{ echo "Cleaning up copter_sitl.sh"; killall xterm && killall mavproxy.py; }' INT
