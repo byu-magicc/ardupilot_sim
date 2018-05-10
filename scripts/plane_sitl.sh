9f6273c32837d795bcbc7afea940905f1a0bd4e6
Devon Morris <devonmorris1992@gmail.com>
2018-05-10 12:47:43 -0600

    initial commit for plane sitl

---
+++ scripts/plane_sitl.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Pass in lat lon of starting point (origin in Gazebo)
+# with syntax: 40.267987,-111.635558 (rock canyon park)
+LATLON=$1
+
+# Catch CTRL+C and do SITL cleanup.
+trap '{ echo "Cleaning up plane_sitl.sh"; killall xterm && killall mavproxy.py; }' INT
+
+CMD="sim_vehicle.py -v ArduPlane -f plane -l $LATLON,0,0 --map --console"
+xterm -fa monospace -fs 12 -n PlaneSITL -T PlaneSITL -hold -geometry 100x20 -e $CMD
