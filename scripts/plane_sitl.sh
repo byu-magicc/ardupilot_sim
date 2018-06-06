f5806600e8f11c98bdf20b4420c8b5e4a17599a8
Devon Morris <devonmorris1992@gmail.com>
2018-06-06 16:37:08 -0600

    Ardupilot is now flying a 4 channel plane (cessna) with waypoints

--- scripts/plane_sitl.sh
+++ scripts/plane_sitl.sh
@@ -7,5 +7,5 @@ LATLON=$1
 # Catch CTRL+C and do SITL cleanup.
 trap '{ echo "Cleaning up plane_sitl.sh"; killall xterm && killall mavproxy.py; }' INT
 
-CMD="sim_vehicle.py -v ArduPlane -f plane -l $LATLON,0,0 --map --console"
+CMD="sim_vehicle.py -v ArduPlane -f gazebo-zephyr $LATLON,0,0 --map --console"
 xterm -fa monospace -fs 12 -n PlaneSITL -T PlaneSITL -hold -geometry 100x20 -e $CMD
