74a694cd57ae001b8449b6ec8b6769198b7a5a5f
Parker Lusk <parkerclusk@gmail.com>
2018-08-18 21:08:17 -0600

    better trapping -- on any type of exit do cleanup

--- scripts/copter_sitl.sh
+++ scripts/copter_sitl.sh
@@ -4,8 +4,8 @@
 # with syntax: 40.267987,-111.635558 (rock canyon park)
 LATLON=$1
 
-# Catch CTRL+C and do SITL cleanup.
-trap '{ echo "Cleaning up copter_sitl.sh"; killall xterm && killall mavproxy.py; }' INT
+# Catch CTRL+C (and other signals) and do SITL cleanup.
+trap '{ echo "Cleaning up copter_sitl.sh"; killall xterm && killall mavproxy.py; }' EXIT
 
 CMD="sim_vehicle.py -v ArduCopter -f gazebo-iris -l $LATLON,0,0 --map --console"
 xterm -fa monospace -fs 12 -n CopterSITL -T CopterSITL -hold -geometry 100x20 -e $CMD
--- scripts/plane_sitl.sh
+++ scripts/plane_sitl.sh
@@ -4,8 +4,8 @@
 # with syntax: 40.267987,-111.635558 (rock canyon park)
 LATLON=$1
 
-# Catch CTRL+C and do SITL cleanup.
-trap '{ echo "Cleaning up plane_sitl.sh"; killall xterm && killall mavproxy.py; }' INT
+# Catch CTRL+C (and other signals) and do SITL cleanup.
+trap '{ echo "Cleaning up plane_sitl.sh"; killall xterm && killall mavproxy.py; }' EXIT
 
 CMD="sim_vehicle.py -v ArduPlane -f gazebo-zephyr $LATLON,0,0 --map --console"
 xterm -fa monospace -fs 12 -n PlaneSITL -T PlaneSITL -hold -geometry 100x20 -e $CMD
