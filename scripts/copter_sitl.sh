084f6d55150169cc279e52143e2df48b63166cd3
Parker Lusk <parkerclusk@gmail.com>
2018-08-18 23:45:13 -0600

    it's a trap!

--- scripts/copter_sitl.sh
+++ scripts/copter_sitl.sh
@@ -5,7 +5,7 @@
 LATLON=$1
 
 # Catch CTRL+C (and other signals) and do SITL cleanup.
-trap '{ echo "Cleaning up copter_sitl.sh"; killall xterm && killall mavproxy.py; }' EXIT
+trap '{ echo "Cleaning up copter_sitl.sh"; kill -f xterm; kill -f mavproxy.py; }' EXIT
 
 CMD="sim_vehicle.py -v ArduCopter -f gazebo-iris -l $LATLON,0,0 --map --console"
 xterm -fa monospace -fs 12 -n CopterSITL -T CopterSITL -hold -geometry 100x20 -e $CMD
